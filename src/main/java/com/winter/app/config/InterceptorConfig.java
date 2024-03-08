ea4af039634a0b3e598224914b080fc9928f9ee7
gohyo04 <gohyo7@gmail.com>
2024-03-08 11:40:30 +0900

    message1

--- src/main/java/com/winter/app/config/InterceptorConfig.java
+++ src/main/java/com/winter/app/config/InterceptorConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 import com.winter.app.interceptors.LoginInterceptor;
 import com.winter.app.interceptors.TestInterceptor;
@@ -14,11 +15,21 @@ public class InterceptorConfig implements WebMvcConfigurer{
 	@Autowired
 	private TestInterceptor testInterceptor;
 	
+	@Autowired
+	private LoginInterceptor loginInterceptor;
+	
+	@Autowired
+	private LocaleChangeInterceptor localeChangeInterceptor;
+	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		// 어떤 URL이 왔을 때 어떤 Interceptor를 거치게 할 것 인가
+//		registry.addInterceptor(testInterceptor)
+//				.addPathPatterns("/notice/*")
+//				.excludePathPatterns("/notice/add");
 		
+		registry.addInterceptor(localeChangeInterceptor)
+				.addPathPatterns("/**");
 	}
 
-	
 }
--- src/main/java/com/winter/app/interceptors/LoginInterceptor.java
+++ src/main/java/com/winter/app/interceptors/LoginInterceptor.java
@@ -8,16 +8,5 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class LoginInterceptor implements WebMvcConfigurer{
 
-//	@Autowired
-//	private TestInterceptor testInterceptor;
-//	
-//	
-//	
-//	@Override
-//	public void addInterceptors(InterceptorRegistry registry) {
-//		// 어떤 URL이 왔을 때 어떤 Interceptor를 거치게 할 것 인가
-//		registry.addInterceptor(testInterceptor)
-//				.addPathPatterns("/notice/*")
-//				.excludePathPatterns("/notice/add");
-//	}
+
 }
